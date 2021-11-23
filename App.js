8addb58615f9fbf37d624b83cc9f02bc13bbb2e7
Wendel Isaac <58012282+wendelisc12@users.noreply.github.com>
2021-11-23 00:45:04 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { StyleSheet, View, Text, TextInput, Button, Image, Linking } from 'react-native';
+import React, {useState} from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -9,13 +8,31 @@ import register from './src/screens/register';
 import listaContatos from './src/screens/listaContatos';
 import cadastroDeUsuario from './src/screens/cadastroDeUsuario';
 import alterarExcluirContatos from './src/screens/alterarExcluirContatos';
+import { initializeApp } from "firebase/app";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBOmaPnRLL6oa4aoIJsb7h-5K7IhSsdtJI",
+    authDomain: "logincadastro-wendel.firebaseapp.com",
+    projectId: "logincadastro-wendel",
+    storageBucket: "logincadastro-wendel.appspot.com",
+    messagingSenderId: "221760676233",
+    appId: "1:221760676233:web:1256cddf5ac9172b170158",
+    measurementId: "G-2CN9Q607Z9"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="login">
+      <Stack.Navigator initialRouteName="listaContatos">
         <Stack.Screen name="login" component={login} options={{title: "Login"}} />
         <Stack.Screen name="register" component={register} options={{title: "Registrar"}}/>
         <Stack.Screen name="listaContatos" component={listaContatos} options={{title: "Contato"}} />
